fa3657811e115b76685d2a09ae778f5f31e37a0f
github akito-10 <akt.fukuda@gmail.com>
2022-10-03 20:31:42 +0900

    🔥

--- build/index.js
+++
@@ -1,6 +0,0 @@
-define(["require", "exports", "./sort/bogo"], function (require, exports, bogo_1) {
-    "use strict";
-    Object.defineProperty(exports, "__esModule", { value: true });
-    const testArray = [1, 3, 9, 4, 6];
-    console.log("bogo", (0, bogo_1.bogo)(testArray));
-});
