da3192f97b84ac647a33bd74cb4b531e68286ffe
github akito-10 <akt.fukuda@gmail.com>
2022-10-03 19:40:25 +0900

    ✨ 並べ替える数字をランダムに

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,10 @@
 import { bogo } from "./sort/bogo";
 
-const testArray = [1, 3, 9, 4, 6];
+const TEST_NUMBER_RANGE = 100;
+const testArray = Array(8)
+  .fill(0)
+  .map(() => {
+    return Math.floor(Math.random() * TEST_NUMBER_RANGE);
+  });
 
 console.log("bogo", bogo(testArray));
