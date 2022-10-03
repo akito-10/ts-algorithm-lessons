573c39e3354fd7ea5fea4b76334120ba9a6e3eb4
github akito-10 <akt.fukuda@gmail.com>
2022-10-03 20:13:49 +0900

    🐛 0なし

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ const TEST_NUMBER_RANGE = 100;
 const testArray = Array(5)
   .fill(0)
   .map(() => {
-    return Math.floor(Math.random() * TEST_NUMBER_RANGE);
+    return Math.floor(Math.random() * TEST_NUMBER_RANGE) + 1;
   });
 
 calSortTime("bogo", bogo, testArray);
